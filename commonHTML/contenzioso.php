fbf638fbd2272faa9b2490e0e00caf5733d49ad2
ElleLuca <ldona@rendertech.com>
2016-01-13 10:28:50 -0500

    contenzioso EN, IT

--- commonHTML/contenzioso.php
+++ commonHTML/contenzioso.php
@@ -10,7 +10,7 @@
 
     <div id="contenzioso"> <?php include('contenzioso_table.html'); ?> </div>
 
-    <p class="LcustomTranslation" id="F291">This table only lists those cases that have reached the General Court or the Court of Justice. The great majority of cases take place before national Courts, other European institutions, or follow administrative proceedings.
+    <p class="LcustomTranslation" id="F291">This table only lists those cases that have reached the General Court or the Court of Justice. The great majority of cases are heard before national Courts or follow administrative proceedings before EU and/or national institutions.
     </p>
 
     <p class="notranslate"><b>Nota Bene</b></p>
@@ -19,11 +19,11 @@
     </p>
 
     <ul>
-      <li class="LcustomTranslation" id="F293">the letter <b>T</b> to indicate proceedings before the <b>Tribunal</b> (until November 30, 2009 called Court of First Instance);
+      <li class="LcustomTranslation" id="F293">the letter <b>T</b> to indicate proceedings before the <b>General Court</b> (until November 30, 2009 called Court of First Instance);
       </li>
-      <li class="LcustomTranslation" id="F294">the letter <b>C</b> (or no letter) to indicate proceedings before the <b><span>Court of&nbsp;Justice </span></b>;
+      <li class="LcustomTranslation" id="F294">the letter <b>C</b> (or no letter) to indicate proceedings before the <b><span>Court of&nbsp;Justice</span></b>;
       </li>
-      <li class="LcustomTranslation" id="F295">the letter <b>P</b> to indicate an appeal proceeding before the <b>Court of Justice against decisions of the&nbsp;Court</b>;
+      <li class="LcustomTranslation" id="F295">the letter <b>P</b> to indicate appeal proceedings before the <b>Court of Justice against decisions of the General Court</b>;
       </li>
       <li class="LcustomTranslation" id="F296">the year of rooting of the case;</li>
       <li class="LcustomTranslation" id="F297">the subject matter and any clarifications.</li>
