b285d6e7f01c3e8b637017f939f59dc92989c6cc
rufanmirganiyev <rufanmirganiyev@gmail.com>
2023-06-15 16:08:55 -0400

    Updated login.php and added new connection file.

--- admin/login.php
+++ admin/login.php
@@ -1,138 +1,72 @@
-<?php
-session_start();  // do not include c_session.php as this page is not protected
-
-$year = time() + 31536000;
-if ($_POST['remember']) {
-    setcookie('remember_me', $_POST['username'], $year);
-    setcookie('remember_me2', $_POST['password'], $year);
-}
+<?php 
+session_start();
 
 if (isset($_POST["submit"])) {
-    $_SESSION['uname'] = $_POST["username"]; //we need uname for access to table
-    $_SESSION['uname_long'] = "z1bb6fc8_" . $_SESSION['uname']; //we need uname_long for access to database
+    $_SESSION['uname'] = $_POST["username"];
+    $_SESSION['uname_long'] = "z1bb6fc8_" . $_SESSION['uname'];
     $_SESSION['psw'] = $_POST["password"];
 
-    if (!$_POST['remember']) {
-        if (isset($_COOKIE['remember_me'])) {
-            $past = time() - 100;
-            setcookie('remember_me', "", $past);
-        }
-        if (isset($_COOKIE['remember_me2'])) {
-            $past = time() - 100;
-            setcookie('remember_me2', "", $past);
+    include_once("connection.php");
+
+    if(isset($connection)){
+        $result = $connection->query("SELECT * FROM accounts WHERE username='".$_SESSION['uname']."' limit 1");
+
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $_SESSION['type'] = $row['type'];
+
+            if(in_array($row['type'], ["Admin", "Maintainer", "Programmer"])) {
+                header("location: posts.php");
+                exit();
+            } else {
+//                $_SESSION['login_error'] = 'Invalid user type.';
+                header("location: ../it/index.php");
+            }
+        } else {
+            $_SESSION['login_error'] = 'Invalid username or password.';
         }
     }
+}
 
-            
-include_once("c_connection.php");
-
-
-    /* Establish connection -- else 'error' */
-//$connection = mysqli_connect($host, $user, $pass, $db);
-//  
-//        if (!$connection)
-//        {
-////            header('location: login.php');
-//            die('Could not connect: ' . mysqli_error());
-//        }
-        mysqli_select_db($connection,$db);
-        
-        $result=mysqli_query($connection,"SELECT * FROM accounts WHERE username='".$_SESSION['uname']."'limit 1");
-        $row = mysqli_fetch_assoc($result);
-        //check the user type and save user name in session
-        
-        
-
-    //check the user type and save user name in session        
-
-         $_SESSION['type']=$row['type'];
-        $_SESSION["Last_Activity"]=time(); 
-        header("location: posts.php");
-        if(mysqli_num_rows($result)==1 && $row['type']=="Admin" || $row['type']=="Maintainer" ){
-            
-            exit();
-            mysqli_close($connection);
-        };
-    
-     
-        if(mysqli_num_rows($result)==1 && $row['type']=="viewer"){
-            exit();
-            mysqli_close($connection);
-        };
-        if(mysqli_num_rows($result)==1 && $row['type']=="Maintainer"){
-            exit();
-            mysqli_close($connection);
-        };
-        
-       
-    }
-    
 ?>
 
-
-
-<html>
-     <head>
-        <meta charset="utf-8" />
-        <?php
-        include_once("head_section.php");
-        ?> 
-        <!--All page specific code goes below this line -->
-
-
-    </head
-    <body>
-        <div>
-    <table style="margin-bottom: 0px;">
-        <tbody>
-            <tr>
-                <td id="left-header" width="80%">
-                    <div id="stars">
-                        <!-- creates a hidden div on top of the page where the stars are displayed
-                             right clicking on the div will lead to a new page -->
-                        <div oncontextmenu="javascript:window.location.href = '../privato/index.php';" 
-                             style="opacity: 0; width: 50px; height:50px; left: 23px; cursor:default; 
-                             background-color:red; position: absolute;"></div>
-
-                        <h1 style="display:inline-block">Dovislex </h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <h2 class="text-center">Dovislex</h2>
+                <h4 class="text-center">Manage Posts</h4>
+                <?php if(isset($_SESSION['login_error'])): ?>
+                    <p class="alert alert-danger"><?= $_SESSION['login_error']; ?></p>
+                    <?php unset($_SESSION['login_error']); ?>
+                <?php endif; ?>
+                <form action="login.php" method="POST">
+                    <div class="form-group">
+                        <label for="username">User</label>
+                        <input type="text" class="form-control" name="username" value="">
                     </div>
-                    <h4>Manage Posts</h4>
-                </td>
-             
-            </tr>
-        </tbody>
-    </table>
-
-
-    <!--
-    <div class="middle-bar"></div> -->
-</div> 
-
-        <form action="login.php" method="POST">
-                User: </br>
-                <input type="text" name="username" value="<?php 
-                    
-                    echo $_COOKIE['remember_me'];
-                ?>"><br/>
-                Password<br/>
-                <input type="password" name="password" value="<?php
-                
-                    echo $_COOKIE['remember_me2']; 
-                
-                ?>"><br/>
-                <input type="submit" name="submit"value="Login">
-                <br>
-                <br>
-                <input type="checkbox" name="remember" id="remember" value="1" <?php 
-                if(isset($_COOKIE['remember_me'])&&isset($_COOKIE['remember_me2'])) {
-                    echo 'checked="checked"';
-                    }
-                    else {
-                        echo '';
-                    }
-                ?>>
-                <label for="remember">Remember me</label>
-            </form>
-        <p> <br>This page is for internal use or authorized clients only.</p>
-    </body>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password" value="">
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" name="remember" id="remember" value="1">
+                        <label class="form-check-label" for="remember">Remember me</label>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary">Login</button>
+                </form>
+                <p class="mt-3">This page is for internal use or authorized clients only.</p>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
 </html>
