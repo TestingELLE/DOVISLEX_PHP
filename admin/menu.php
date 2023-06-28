d0950305242b02a7d813cae4291602048c47bcff
rufanmirganiyev <rufanmirganiyev@gmail.com>
2023-06-28 19:35:09 -0400

    Updated menu.css and posts.css to resolve styling issues for better user interface.

--- admin/menu.php
+++ admin/menu.php
@@ -1,20 +1,22 @@
 <?php include("connection.php"); ?>
 <!-- menu.php -->
 <link rel="stylesheet" href="css/menu.css">
-<div class="menu">
-    <div class="card">
-        <div class="card-header">
-            <h2 style="color: red">Dovislex -
-                <?php echo isset($_SESSION['type']) ? $_SESSION['type'] : ''; ?></h2>
+<div class="menu-container">
+    <div class="menu">
+        <div class="card">
+            <div class="card-header">
+                <h2 style="color: #0335fc">Dovislex -
+                    <?php echo isset($_SESSION['type']) ? $_SESSION['type'] : ''; ?></h2>
 
-        </div>
-        <div class="card-content">
-            <a href="create_post.php">Create Posts</a>
-            <a href="posts.php">Manage Posts</a>
-            <a href="../en/index.php?page=news_sql">View News Page</a>
-            <a href="logout.php" class="logout-btn">Logout - User: 
-                <span style="color: red"><?php echo isset($_SESSION['uname']) ? $_SESSION['uname'] : ''; ?></span>
-            </a>
+            </div>
+            <div class="card-content">
+                <a href="create_post.php">Create Posts</a>
+                <a href="posts.php">Manage Posts</a>
+                <a href="../en/index.php?page=news_sql">View News Page</a>
+                <a href="logout.php" class="logout-btn">Logout - User: 
+                    <span style="color: #0335fc"><?php echo isset($_SESSION['uname']) ? $_SESSION['uname'] : ''; ?></span>
+                </a>
+            </div>
         </div>
     </div>
 </div>
